a529aaf9786cba3b0efd80fe7111d1c6f4797809
miuer <57321732+miuer@users.noreply.github.com>
2021-08-02 12:21:37 +0800

    init main.go

--- main.go
+++ main.go
@@ -2,16 +2,8 @@ package main
 
 import (
 	"fmt"
-	"sync/atomic"
 )
 
 func main() {
-	var sl atomic.Value
-	sl.Store([]int{1, 3, 5, 7})
-	store(sl)
-	fmt.Printf("value: %+d \n", sl.Load())
-}
-
-func store(sl atomic.Value) {
-	sl.Store([]int{2, 4, 6, 8})
+	fmt.Println("hello world")
 }
